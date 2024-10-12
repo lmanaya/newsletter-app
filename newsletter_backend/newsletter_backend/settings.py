dbd6e4216637e150ee108f8273664af665ea9c0c
Liyeira Anaya <lmanaya990320@gmail.com>
2024-10-12 17:41:48 -0500

    chore: Migrate from SQLite to PostgreSQL

--- newsletter_backend/newsletter_backend/settings.py
+++ newsletter_backend/newsletter_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'nombre_app',
+    'newsletters',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'newsletter_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": environ.get('POSTGRES_DB'),
+        "USER": environ.get('POSTGRES_USER'),
+        "PASSWORD": environ.get('POSTGRES_PASSWORD'),
+        "HOST": environ.get('POSTGRES_HOST'),
+        "PORT": environ.get('POSTGRES_PORT'),
     }
 }
 
