e6a496e08335a0193b48360ef46beee038ae8eb0
Liyeira Anaya <lmanaya990320@gmail.com>
2024-10-14 07:51:59 -0500

    feat: Create models of documents, templates and email sends

---
+++ newsletter_backend/email_sends/constants.py
@@ -0,0 +1,9 @@
+PENDING_STATUS = "pending"
+SENDED_STATUS = "sended"
+FAILED_STATUS = "failed"
+
+STATUS_EMAIL_CHOICES = [
+    (PENDING_STATUS, 'Pendiente'),
+    (SENDED_STATUS, 'Enviado'),
+    (FAILED_STATUS, 'Falló')
+]
