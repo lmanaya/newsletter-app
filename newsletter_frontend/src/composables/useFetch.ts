dab05e60c4df0c32ef064b40bbef221d62e3a710
Liyeira Anaya <lmanaya990320@gmail.com>
2024-10-16 20:50:41 -0500

    feat: Update fetch hook including data destructured in response

--- newsletter_frontend/src/composables/useFetch.ts
+++ newsletter_frontend/src/composables/useFetch.ts
@@ -3,17 +3,21 @@ import { useStore } from 'vuex';
 import api from '@/services/api';
 import { ErrorResponse, PaginatedResponse } from '@/types/api';
 
-export const useFetch = (url: string) => {
+export const useFetch = (url: String) => {
     const data = ref<PaginatedResponse | null>(null);
     const error = ref<ErrorResponse | null>(null);
     const store = useStore();
 
     const loading = computed(() => store.state.loading);
+    const count = computed<number>(() => data.value?.count ?? 0);
+    const previous = computed<String | null>(() => data.value?.previous ? data.value?.previous.split("?")[1] : null);
+    const next = computed<String | null>(() => data.value?.next ? data.value?.next.split("?")[1] : null);
+    const results = computed<[]>(() => data.value?.results ?? []);
 
-    const fetchData = async () => {
+    const fetchData = async (query: String | null = null) => {
         store.dispatch('loading', true);
         try {
-            const response = await api.get(url);
+            const response = await api.get(`${url}${query ? "?" + query : ""}`);
             data.value = response.data;
         } catch (error: any) {
             console.error('Error fetching data', error);
@@ -27,5 +31,13 @@ export const useFetch = (url: string) => {
         fetchData();
     });
 
-    return { loading, error, data, fetchData };
+    return {
+        loading,
+        error,
+        count,
+        next,
+        previous,
+        results,
+        fetchData
+    };
 }
